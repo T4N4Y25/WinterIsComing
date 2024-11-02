12655f1e6439bbb135dac80926621aa17576ee0d
bigchungus <shivolong4@gmail.com>
2024-11-02 18:16:48 +0200

    added switch scene when countdown fhinishes

--- WinterIsComing/Assets/Timer.cs
+++ WinterIsComing/Assets/Timer.cs
@@ -1,17 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; // Needed for scene management
 using TMPro;
+
 public class Timer : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI timerText;
     float elapsedtime;
+    float timeLimit = 60f; // 3 minutes in seconds
+
     // Update is called once per frame
     void Update()
     {
-        elapsedtime += Time.deltaTime;
-        int minutes = Mathf.FloorToInt(elapsedtime / 60);
-        int seconds = Mathf.FloorToInt(elapsedtime % 60);
-        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        // Only update if the elapsed time is less than the time limit
+        if (elapsedtime < timeLimit)
+        {
+            elapsedtime += Time.deltaTime;
+            int minutes = Mathf.FloorToInt(elapsedtime / 60);
+            int seconds = Mathf.FloorToInt(elapsedtime % 60);
+            timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+            // Check if we've reached the time limit
+            if (elapsedtime >= timeLimit)
+            {
+                // Load the next scene
+                
+                SceneManager.LoadScene("BossLevel");
+            }
+        }
     }
 }
